b662a70548efa5a28a2d54d5782968001b06f807
Ben Altieri <chiefspace@gmail.com>
2017-03-23 23:42:24 +0000

    added test class for tests_get_songs

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -28,6 +28,33 @@ class TestAPI(unittest.TestCase):
         # Create folder for test uploads
         os.mkdir(upload_path())
         
+    def test_get_songs(self):
+        file_A = models.File(name='file_A.mp3')
+        song_A = models.Song(file_=file_A)
+        file_B = models.File(name='file_B.mp3')
+        song_B = models.Song(file_=file_B)
+
+        session.add_all([file_A, file_B, song_A, song_B])
+        session.commit()
+
+        response = self.client.get(
+            '/api/songs',
+            headers=[("Accept", "application/json")])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+
+        data = json.loads(response.data.decode('ascii'))
+        self.assertEqual(len(data), 2)
+
+        response_song_A, response_song_B = data
+        self.assertEqual(response_song_A['id'], song_A.id)
+        self.assertEqual(response_song_A['file']['id'], song_A.file_.id)
+        self.assertEqual(response_song_A['file']['name'], song_A.file_.name)
+        self.assertEqual(response_song_B['id'], song_B.id)
+        self.assertEqual(response_song_B['file']['id'], song_B.file_.id)
+        self.assertEqual(response_song_B['file']['name'], song_B.file_.name)
+        
     def test_get_uploaded_file(self):
         path = upload_path('test.txt')
         with open(path, "wb") as f:
